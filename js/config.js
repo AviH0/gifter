9618400123ff4c5603e2fbf886ecd216d4effea8
AviH <avinoam@hershler.com>
2026-01-25 21:03:44 +0200

    Added paybox and bit links

--- js/config.js
+++ js/config.js
@@ -18,12 +18,12 @@ const config = {
     gifts: [
         {
             name: { en: "Bit", he: "ביט" },
-            url: ["https://bit.co.il/1", "https://bit.co.il/2"],
+            url: ["https://www.bitpay.co.il/app/me/AFA13A54-5E81-9D62-B2F5-1582F5C46E96D260"],
             logo: "https://play-lh.googleusercontent.com/SovCyuQk4A0se4hmrQbf1OJXznHlfseeq9YtPh_WHe3lVMFzZcXqBGfW2J_1yYI6yQ=w240-h480-rw"
         },
         {
             name: { en: "Paybox", he: "פייבוקס" },
-            url: ["https://paybox.co.il/1", "https://paybox.co.il/1"],
+            url: ["https://links.payboxapp.com/WXnmoi0ed0b"],
             logo: "https://yt3.googleusercontent.com/ytc/AIdro_lhy54u_HxaHGFBzqnJbd6oh7cVzfpbDdxBsCFn4ohvlss=s900-c-k-c0x00ffffff-no-rj"
         },
     ]
